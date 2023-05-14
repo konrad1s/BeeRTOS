d740fda54891643b383cd21c6fe218e883fa28d9
Konrad Sikora <46894716+konrad516@users.noreply.github.com>
2023-05-14 17:32:33 +0200

    Added mutex with task priority inversion and UT

---
+++ BeeRTOS/Src/BeeRTOS_mutex.c
@@ -0,0 +1,107 @@
+/******************************************************************************************
+ * @brief OS mutex source file
+ * @file BeeRTOS_mutex.c
+ * ****************************************************************************************/
+
+/******************************************************************************************
+ *                                        INCLUDES                                        *
+ ******************************************************************************************/
+
+#include "BeeRTOS_mutex.h"
+#include "BeeRTOS_task.h"
+#include "BeeRTOS_assert.h"
+
+/******************************************************************************************
+ *                                         DEFINES                                        *
+ ******************************************************************************************/
+
+/******************************************************************************************
+ *                                        TYPEDEFS                                        *
+ ******************************************************************************************/
+
+typedef struct
+{
+    os_task_t *owner;
+    uint32_t count;
+    uint32_t tasks_blocked;
+    uint8_t owner_priority;
+} os_mutex_t;
+
+/******************************************************************************************
+ *                                        VARIABLES                                       *
+ ******************************************************************************************/
+
+extern os_task_t *os_tasks[OS_TASK_MAX];
+static os_mutex_t mutexes[BEERTOS_MUTEX_ID_MAX];
+
+/******************************************************************************************
+ *                                        FUNCTIONS                                       *
+ ******************************************************************************************/
+
+void os_mutex_lock(os_mutex_id_t id, uint32_t timeout)
+{
+    BEERTOS_ASSERT(id < BEERTOS_MUTEX_ID_MAX, OS_MODULE_ID_MUTEX, OS_ERROR_INVALID_PARAM);
+
+    os_mutex_t *mutex = &mutexes[id];
+    os_task_t *task = os_get_current_task();
+
+    os_disable_all_interrupts();
+
+    if (NULL == mutex->owner)
+    {
+        mutex->owner = task;
+        mutex->count++;
+    }
+    else
+    {
+        if (mutex->owner->priority < task->priority)
+        {
+            mutex->owner_priority = mutex->owner->priority;
+            mutex->owner->priority = task->priority;
+        }
+        mutex->tasks_blocked |= (1U << task->priority - 1U);
+        os_delay(timeout);
+        os_sched();
+    }
+    os_enable_all_interrupts();
+}
+
+void os_mutex_unlock(os_mutex_id_t id)
+{
+    BEERTOS_ASSERT(id < BEERTOS_MUTEX_ID_MAX, OS_MODULE_ID_MUTEX, OS_ERROR_INVALID_PARAM);
+
+    os_mutex_t *mutex = &mutexes[id];
+    os_task_t *task = os_get_current_task();
+
+    os_disable_all_interrupts();
+
+    if (mutex->owner == task)
+    {
+        mutex->count--;
+        if (mutex->count == 0U)
+        {
+            mutex->owner->priority = mutex->owner_priority;
+
+            uint32_t task_id = OS_LOG2(mutex->tasks_blocked) & 0xFFU;
+            os_task_t* task = os_tasks[task_id];
+            if (task_id != 0U)
+            {
+                mutex->owner = os_tasks[task_id];
+                mutex->count++;
+                mutex->tasks_blocked &= ~(1U << task_id - 1U);
+                os_task_release(task_id);
+                os_sched();
+            }
+            else
+            {
+                mutex->owner = NULL;
+            }
+        }
+    }
+    else
+    {
+        /* TODO assert */
+    }
+
+    os_enable_all_interrupts();
+}
---
+++ UnitTests/TestSets/ut_04_mutexes.c
@@ -0,0 +1,55 @@
+#include "ut_utils.h"
+
+uint32_t tasks_cnt[3];
+
+/* Lowest priority task */
+void ut_task_mutex_1(void *arg)
+{
+    /* Lock the mutex */
+    os_mutex_lock(MUTEX_ONE, 1000);
+    ut_blocking_delay(20);
+
+    /* priority should be inherited */
+    ut_blocking_delay(80);
+    os_mutex_unlock(MUTEX_ONE);
+
+    tasks_cnt[0]++;
+    
+    while(1)
+    {
+
+    }
+}
+
+/* Medium priority task */
+void ut_task_mutex_2(void *arg)
+{
+    tasks_cnt[1]++;
+    os_task_delete();
+}
+
+/* Highest priority task */
+void ut_task_mutex_3(void *arg)
+{
+    /* Let the other tasks to lock the mutex */
+    os_delay(5);
+
+    tasks_cnt[2]++;
+
+    os_mutex_lock(MUTEX_ONE, 2000);
+    
+    tasks_cnt[2]++;
+    ut_blocking_delay(10);
+    os_task_delete();
+}
+
+void TEST_mutexes(void)
+{
+    PRINT_UT_BEGIN();
+
+    os_delay(300);
+
+    TEST_ASSERT_EQUAL(1, tasks_cnt[0]);
+    TEST_ASSERT_EQUAL(1, tasks_cnt[1]);
+    TEST_ASSERT_EQUAL(2, tasks_cnt[2]);
+}
