4eca925bb55a719f12a450e54faf8ae0ff51c0d2
Konrad Sikora <46894716+konrad516@users.noreply.github.com>
2023-06-04 19:29:50 +0200

    Add UT for binary sempahore

--- UnitTests/TestSets/ut_03_semaphores.c
+++ UnitTests/TestSets/ut_03_semaphores.c
@@ -1,37 +1,72 @@
 #include "ut_utils.h"
 
-void TEST_semaphores(void)
+void TEST_couting_semaphores(void)
 {
-    PRINT_UT_BEGIN();
-
+    bool ret;
     /* Test 1: wait for semaphore with timeout 0, SEMAPHORE_UT_INITIAL_COUNT times
        Expected result: true */
     for (int i = 0; i < SEMAPHORE_UT_INITIAL_COUNT; i++)
     {
-        bool ret = os_semaphore_wait(SEMAPHORE_UT, 0);
+        ret = os_semaphore_wait(SEMAPHORE_UT1, 0);
         TEST_ASSERT(ret == true);
     }
 
     /* Test 2: wait for semaphore with timeout 0, one more time
        Expected result: false */
-    bool ret = os_semaphore_wait(SEMAPHORE_UT, 0);
+    ret = os_semaphore_wait(SEMAPHORE_UT1, 0);
     TEST_ASSERT(ret == false);
 
      /* Test 2: wait for semaphore with timeout 1000, one more time
        Expected result: false and task blocked for 1000 ticks */
     uint32_t start = os_get_tick_count();
-    ret = os_semaphore_wait(SEMAPHORE_UT, 1000);
+    ret = os_semaphore_wait(SEMAPHORE_UT1, 1000);
     uint32_t end = os_get_tick_count();
     TEST_ASSERT(ret == false);
     TEST_ASSERT(end - start == 1000);
 
     /* Test 3: signal semaphore
        Expected result: true */
-    ret = os_semaphore_signal(SEMAPHORE_UT);
+    ret = os_semaphore_signal(SEMAPHORE_UT1);
     TEST_ASSERT(ret == true);
 
     /* Test 4: wait for semaphore with timeout 0, one more time
        Expected result: true */
-    ret = os_semaphore_wait(SEMAPHORE_UT, 0);
+    ret = os_semaphore_wait(SEMAPHORE_UT1, 0);
     TEST_ASSERT(ret == true);
+}
+
+void TEST_binary_semaphores(void)
+{
+    bool ret;
+
+    /* Test 1: wait for semaphore with timeout 0, 1 time
+       Expected result: false */
+    ret = os_semaphore_wait(SEMAPHORE_UT2, 0);
+    TEST_ASSERT(ret == false);
+
+    /* Test 2: signal semaphore
+       Expected result: true */
+    ret = os_semaphore_signal(SEMAPHORE_UT2);
+
+    /* Test 3: signal semaphore
+       Expected result: false */
+    ret = os_semaphore_signal(SEMAPHORE_UT2);
+    TEST_ASSERT(ret == false);
+
+    /* Test 4: wait for semaphore with timeout 0, 1 time
+       Expected result: true */
+    ret = os_semaphore_wait(SEMAPHORE_UT2, 0);
+    TEST_ASSERT(ret == true);
+}
+
+void TEST_semaphores(void)
+{
+    PRINT_UT_BEGIN();
+
+    for (int i = 0; i < 10; i++)
+    {
+        os_semaphores_init();
+        TEST_couting_semaphores();
+        TEST_binary_semaphores();
+    }
 }
