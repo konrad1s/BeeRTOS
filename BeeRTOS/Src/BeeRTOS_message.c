a43e7d3fe680d5fead3dae47807f2ced12afaf20
Konrad Sikora <46894716+konrad516@users.noreply.github.com>
2023-06-12 22:36:03 +0200

    OS_message update code format and comments

--- BeeRTOS/Src/BeeRTOS_message.c
+++ BeeRTOS/Src/BeeRTOS_message.c
@@ -21,6 +21,7 @@
  *                                        TYPEDEFS                                        *
  ******************************************************************************************/
 
+/*! Structure describing a message (created using os_queue_t structure) */
 typedef struct
 {
     os_queue_t *queue;
@@ -31,27 +32,29 @@ typedef struct
 /******************************************************************************************
  *                                        VARIABLES                                       *
  ******************************************************************************************/
+
 extern os_queue_t os_queues[OS_MSG_QUEUE_ID_MAX];
 extern os_task_t *os_tasks[OS_TASK_MAX];
 
-static os_message_t messages[OS_MESSAGE_ID_MAX];
+/*! List of all messages */
+static os_message_t os_messages[OS_MESSAGE_ID_MAX];
 
 /******************************************************************************************
  *                                        FUNCTIONS                                       *
  ******************************************************************************************/
 
-void os_messages_init(void)
+void os_message_init(void)
 {
     /*! X-Macro to initialize all messages */
     #undef OS_MESSAGE
-    #define OS_MESSAGE(name, count, size) \
-        messages[name].queue = &os_queues[name + BEERTOS_QUEUE_ID_MAX]; \
-        messages[name].item_size = size; \
-        messages[name].tasks_blocked = 0U;
+    #define OS_MESSAGE(name, count, size)       \
+        os_messages[name].queue = &os_queues[name + BEERTOS_QUEUE_ID_MAX]; \
+        os_messages[name].item_size = size;     \
+        os_messages[name].tasks_blocked = 0U;
     
-    #define BEERTOS_MESSAGES_INIT OS_MESSAGES_LIST
+    #define BEERTOS_MESSAGES_INIT_ALL() OS_MESSAGES_LIST
 
-    BEERTOS_MESSAGES_INIT
+    BEERTOS_MESSAGES_INIT_ALL();
 }
 
 bool os_message_send(os_message_id_t id, void *data, uint32_t timeout)
@@ -60,20 +63,23 @@ bool os_message_send(os_message_id_t id, void *data, uint32_t timeout)
     BEERTOS_ASSERT(data != NULL, OS_MODULE_ID_MESSAGE, OS_ERROR_NULLPTR);
 
     bool ret = false;
+    os_message_t *const msg = &os_messages[id];
 
-    os_message_t *msg = &messages[id];
-
+    /* Check if message can be pushed instantly */
     if (true == os_queue_push(id + BEERTOS_QUEUE_ID_MAX, data, msg->item_size))
     {
         ret = true;
     }
     else
     {
+        /* If timeout is set, wait for the other task to pop the message */
         if (0U != timeout)
         {
             msg->tasks_blocked |= (1U << (os_get_current_task()->priority - 1U));
             os_delay(timeout);
 
+            /* Try to push the message again, if it fails, it means that the timeout expired
+               and the message was not popped by the other task */
             if (true == os_queue_push(id + BEERTOS_QUEUE_ID_MAX, data, msg->item_size))
             {
                 ret = true;
@@ -91,14 +97,16 @@ bool os_message_receive(os_message_id_t id, void *data, uint32_t timeout)
 
     bool ret = false;
 
-    os_message_t *msg = &messages[id];
+    os_message_t *const msg = &os_messages[id];
 
     // os_disable_all_interrupts();
 
+    /* Check if message can be popped instantly */
     if (true == os_queue_pop(id + BEERTOS_QUEUE_ID_MAX, data, msg->item_size))
     {
         ret = true;
 
+        /* If there are tasks blocked on this message, release the highest priority one */
         if (0U != msg->tasks_blocked)
         {
             os_task_t* task = os_tasks[OS_LOG2(msg->tasks_blocked) & 0xFFU];
--- BeeRTOS/Src/BeeRTOS_message.h
+++ BeeRTOS/Src/BeeRTOS_message.h
@@ -24,6 +24,7 @@
 #undef OS_MESSAGE
 #define OS_MESSAGE(name, ...) name,
 
+/*! Message IDs - generated from OS_MESSAGES_LIST() in BeeRTOS_message_cfg.h */
 typedef enum
 {
     OS_MESSAGES_LIST
@@ -38,7 +39,38 @@ typedef enum
  *                                   FUNCTION PROTOTYPES                                  *
  ******************************************************************************************/
 
+/**
+ * @brief The function initializes the operating system's messages.
+ * It is responsible for setting up any necessary data structures.
+ * Called once (automatically) in os system initialization.
+ *
+ * @param None
+ * @return None
+ */
+void os_message_init(void);
+
+/**
+ * @brief This function sends a message to the specified message queue.
+ * 
+ * @param id - message queue id
+ * @param data - pointer to the data to be sent
+ * @param timeout - maximum time to wait for the message to be sent. If timeout is 0, the function
+ *                  will return immediately false if the message queue is full.
+ * @return true - message sent successfully in the given timeout
+ *         false - message not sent
+ */
 bool os_message_send(os_message_id_t id, void *data, uint32_t timeout);
+
+/**
+ * @brief This function receives a message from the specified message queue.
+ * 
+ * @param id - message queue id
+ * @param data - pointer to the data to be received
+ * @param timeout - maximum time to wait for the message to be received. If timeout is 0, the function
+ *                  will return immediately false if the message queue is empty.
+ * @return true - message received successfully in the given timeout
+ *         false - message not received
+ */
 bool os_message_receive(os_message_id_t id, void *data, uint32_t timeout);
 
 #endif /* __BEERTOS_MESSAGE_H__ */
