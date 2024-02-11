8224fa8bb02dda821580501ac559d68737a8b109
Konrad Sikora <konradsikora98@gmail.com>
2024-02-11 21:55:17 +0100

    Added more smoke tests to the mutex module

--- BeeRTOS/Cfg/BeeRTOS_cfg.h
+++ BeeRTOS/Cfg/BeeRTOS_cfg.h
@@ -22,29 +22,33 @@
  ******************************************************************************************/
 
 /*! Enable/Disable OS features */
-#define BEERTOS_ALARM_MODULE_EN     (true)
-#define BEERTOS_MESSAGE_MODULE_EN   (true)
-#define BEERTOS_QUEUE_MODULE_EN     (true)
-#define BEERTOS_MUTEX_MODULE_EN     (true)
+#define BEERTOS_ALARM_MODULE_EN (true)
+#define BEERTOS_MESSAGE_MODULE_EN (true)
+#define BEERTOS_QUEUE_MODULE_EN (true)
+#define BEERTOS_MUTEX_MODULE_EN (true)
 #define BEERTOS_SEMAPHORE_MODULE_EN (true)
 
 /* Stack size of the idle task in bytes - if callbacks not used you can set 16 bytes */
-#define BEERTOS_IDLE_TASK_STACK_SIZE    (64U)
+#define BEERTOS_IDLE_TASK_STACK_SIZE (64U)
 /* Callback when idle task is initialized (before entering the idle loop) */
-#define BEERTOS_IDLE_TASK_INIT_CB()     {}
+#define BEERTOS_IDLE_TASK_INIT_CB() \
+    {                               \
+    }
 /* Callback when idle task is running (in the idle loop) */
-#define BEERTOS_IDLE_TASK_CB()          {}
+#define BEERTOS_IDLE_TASK_CB() \
+    {                          \
+    }
 
-/* Enable this option to use the stack overflow detection feature. 
+/* Enable this option to use the stack overflow detection feature.
  * When tasks is created, the stack is filled with a pattern. Each context switch
  * the pattern is checked. If the pattern is not the same, then we can assume that
  * the stack has overflowed. */
-#define BEERTOS_USE_TASK_STACK_MONITOR  (true)
+#define BEERTOS_USE_TASK_STACK_MONITOR (true)
 /* Fast metod checks only the last 4 bytes of the stack each context switch. */
-#define BEERTOS_USE_FAST_STACK_MONITOR  (true)
+#define BEERTOS_USE_FAST_STACK_MONITOR (true)
 /* This method checks the OS_TASK_STACK_CHECK_BYTE_COUNT bytes of the stack each context switch. */
-#define BEERTOS_USE_USER_STACK_MONITOR  (false)
-#define OS_TASK_STACK_CHECK_BYTE_COUNT  (10U)
+#define BEERTOS_USE_USER_STACK_MONITOR (false)
+#define OS_TASK_STACK_CHECK_BYTE_COUNT (10U)
 
 /*!
  *  @brief OS tasks configuration list - define your tasks here.
@@ -88,24 +92,21 @@
  *  This configuration defines system tasks, mutexes, and a single alarm task as per
  *  the new capabilities of the BeeRTOS system.
  */
-#define BEERTOS_PRIORITY_LIST()                                             \
-    BEERTOS_TASK(OS_TASK_UT_MAIN, ut_beertos_main_task, 128, true, NULL)    \
-    BEERTOS_ALARM_TASK(OS_ALARM_TASK, 128)                                  \
-    /* Priority test tasks */                                               \
-    BEERTOS_TASK(OS_TASK_PRIO_3, ut_task_priority_highest, 128, true, NULL) \
-    BEERTOS_TASK(OS_TASK_PRIO_2, ut_task_priority_medium, 128, true, NULL)  \
-    BEERTOS_TASK(OS_TASK_PRIO_1, ut_task_priority_lowest, 128, true, NULL)  \
-    /* Mutex test tasks */                                                  \
-    BEERTOS_MUTEX(MUTEX_ONE, 0U)                                            \
-    BEERTOS_MUTEX(MUTEX_TWO, 0U)                                            \
-    BEERTOS_MUTEX(MUTEX_THREE, 0U)                                          \
-    BEERTOS_TASK(OS_TASK_MUTEX_3, ut_task_mutex_3, 128, true, NULL)         \
-    BEERTOS_TASK(OS_TASK_MUTEX_2, ut_task_mutex_2, 128, true, NULL)         \
-    BEERTOS_TASK(OS_TASK_MUTEX_1, ut_task_mutex_1, 128, true, NULL)         \
-    /* Message test tasks */                                                \
-    BEERTOS_TASK(OS_TASK_MSG_2, ut_task_msg_2, 128, false, NULL)            \
+#define BEERTOS_PRIORITY_LIST()                                                 \
+    BEERTOS_TASK(OS_TASK_UT_MAIN, ut_beertos_main_task, 128, true, NULL)        \
+    BEERTOS_ALARM_TASK(OS_ALARM_TASK, 128)                                      \
+    /* Priority test tasks */                                                   \
+    BEERTOS_TASK(OS_TASK_PRIO_3, ut_task_priority_highest, 128, true, NULL)     \
+    BEERTOS_TASK(OS_TASK_PRIO_2, ut_task_priority_medium, 128, true, NULL)      \
+    BEERTOS_TASK(OS_TASK_PRIO_1, ut_task_priority_lowest, 128, true, NULL)      \
+    /* Mutex test tasks */                                                      \
+    BEERTOS_MUTEX(MUTEX_ONE, 0U)                                                \
+    BEERTOS_MUTEX(MUTEX_THREE, 0U)                                              \
+    BEERTOS_TASK(OS_TASK_MUTEX_3, mutex_high_priority_task, 128, false, NULL)   \
+    BEERTOS_TASK(OS_TASK_MUTEX_1, mutex_low_priority_task, 128, false, NULL)    \
+    /* Message test tasks */                                                    \
+    BEERTOS_TASK(OS_TASK_MSG_2, ut_task_msg_2, 128, false, NULL)                \
     BEERTOS_TASK(OS_TASK_MSG_1, ut_task_msg_1, 128, false, NULL)
-
 
 /*! @brief BeeRTOS message list - define your messages here
  * Messages are more specific than queues, they can store only one type of data
@@ -118,11 +119,10 @@
  * @param messages_count - number of messages that can be stored in queue
  * @param message_size - size of single message in bytes
  */
-#define OS_MESSAGES_LIST()              \
-    OS_MESSAGE(MESSAGE_ONE,   2,  8)    \
-    OS_MESSAGE(MESSAGE_TWO,   10, 4)    \
+#define OS_MESSAGES_LIST()         \
+    OS_MESSAGE(MESSAGE_ONE, 2, 8)  \
+    OS_MESSAGE(MESSAGE_TWO, 10, 4) \
     OS_MESSAGE(MESSAGE_THREE, 10, 4)
-
 
 /*! @brief BeeRTOS queue list - define your queues here
  *  Queues are more general than messages, they can store any type of data
@@ -130,37 +130,35 @@
  *  There can be popped any number of elements from queue, as long as queue is not empty.
  *  Queues in BeeRTOS are implemented as circular buffers, if queue is full, new elements
  *  will not overwrite old ones, but will be discarded without task delay.
- * 
+ *
  *  Structure: OS_QUEUE(queue_id, queue size)
  * @param queue_id - queue id (created in os_queue_id_t enum), must be unique
  * @param queue size - queue size in bytes - maximum number of elements that can be stored in queue
  */
-#define BEERTOS_QUEUE_LIST()    \
-    OS_QUEUE(QUEUE_1, 10U)      \
-    OS_QUEUE(QUEUE_2, 10U)      \
-
+#define BEERTOS_QUEUE_LIST() \
+    OS_QUEUE(QUEUE_1, 10U)   \
+    OS_QUEUE(QUEUE_2, 10U)
 
 /*! @brief BeeRTOS semaphore list - define your semaphores here
  * Semaphores are used to synchronize tasks and to protect shared resources from being
  * accessed by more than one task at the same time. Semaphores can be used to implement
  * mutual exclusion, but mutexes are preferred for this purpose. Semaphores do not implement
  * priority inheritance. Semaphores can be used for signaling between tasks.
- * 
+ *
  * Structure: BEERTOS_SEMAPHORE(semaphore_id, initial_count, type)
  * @param semaphore_id - semaphore id (created in os_semaphore_id_t enum), must be unique
  * @param initial_count - initial count of the semaphore
  * @param type - semaphore type (counting or binary)
  */
-#define BEERTOS_SEMAPHORE_LIST()               \
-    BEERTOS_SEMAPHORE(SEMAPHORE_UT1,     10U,   SEMAPHORE_TYPE_COUNTING) \
-    BEERTOS_SEMAPHORE(SEMAPHORE_UT2,     0U,    SEMAPHORE_TYPE_BINARY)   \
-    BEERTOS_SEMAPHORE(SEMAPHORE_TWO,     0U,    SEMAPHORE_TYPE_BINARY)
-
+#define BEERTOS_SEMAPHORE_LIST()                                   \
+    BEERTOS_SEMAPHORE(SEMAPHORE_UT1, 10U, SEMAPHORE_TYPE_COUNTING) \
+    BEERTOS_SEMAPHORE(SEMAPHORE_UT2, 0U, SEMAPHORE_TYPE_BINARY)    \
+    BEERTOS_SEMAPHORE(SEMAPHORE_TWO, 0U, SEMAPHORE_TYPE_BINARY)
 
 /*!
  *  @brief Define your alarms here
  *  @note Structure: BEERTOS_ALARM(alarm_id, callback, autostart, default_period, periodic)
- * 
+ *
  *  @param alarm_id - alarm id (created in os_alarm_id_t enum), must be unique
  *  @param callback - callback function to be called when alarm expires
  *  @param autostart - if TRUE, alarm will be started automatically after OS initialization
@@ -169,10 +167,10 @@
  *  @param periodic - if TRUE, alarm will be periodic, otherwise it will be one-shot,
  *                    set only if autostart is TRUE, otherwise use os_alarm_start with periodic parameter
  */
-#define BEERTOS_ALARM_LIST() \
-    BEERTOS_ALARM(ALARM_ONE,      alarm1_callback, false, 0U, false) \
-    BEERTOS_ALARM(ALARM_TWO,      alarm2_callback, false, 0U, false) \
-    BEERTOS_ALARM(ALARM_THREE,    alarm3_callback, false, 0U, false)
+#define BEERTOS_ALARM_LIST()                                    \
+    BEERTOS_ALARM(ALARM_ONE, alarm1_callback, false, 0U, false) \
+    BEERTOS_ALARM(ALARM_TWO, alarm2_callback, false, 0U, false) \
+    BEERTOS_ALARM(ALARM_THREE, alarm3_callback, false, 0U, false)
 
 /******************************************************************************************
  *                                        TYPEDEFS                                        *
@@ -192,9 +190,8 @@ extern void ut_task_priority_lowest(void *arg);
 extern void ut_task_priority_medium(void *arg);
 extern void ut_task_priority_highest(void *arg);
 
-extern void ut_task_mutex_1(void *arg);
-extern void ut_task_mutex_2(void *arg);
-extern void ut_task_mutex_3(void *arg);
+extern void mutex_low_priority_task(void *arg);
+extern void mutex_high_priority_task(void *arg);
 
 extern void ut_task_msg_1(void *arg);
 extern void ut_task_msg_2(void *arg);
--- SmokeTests/TestSets/ut_04_mutexes.c
+++ SmokeTests/TestSets/ut_04_mutexes.c
@@ -1,47 +1,52 @@
 #include "ut_utils.h"
 
-uint32_t tasks_cnt[3];
-
-/* Lowest priority task */
-void ut_task_mutex_1(void *arg)
+enum
 {
-    /* Lock the mutex */
-    os_mutex_lock(MUTEX_ONE, 1000);
-    ut_blocking_delay(20);
+    UT_TASK_PRIORITY_LOWEST = 0,
+    UT_TASK_PRIORITY_HIGHEST,
+    UT_TASK_PRIORITY_COUNT
+} ut_mutex_tasks_priority_t;
 
-    /* priority should be inherited */
-    ut_blocking_delay(80);
+volatile uint32_t tasks_cnt[UT_TASK_PRIORITY_COUNT];
+
+void reset_counters(void)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        tasks_cnt[i] = 0;
+    }
+}
+
+void mutex_low_priority_task(void *arg)
+{
+    tasks_cnt[UT_TASK_PRIORITY_LOWEST]++;
+
+    os_mutex_lock(MUTEX_ONE, 10000);
+    tasks_cnt[UT_TASK_PRIORITY_LOWEST]++;
+    ut_blocking_delay(20);
     os_mutex_unlock(MUTEX_ONE);
 
-    tasks_cnt[0]++;
+    tasks_cnt[UT_TASK_PRIORITY_LOWEST]++;
+
     while(1)
     {
         os_delay(500);
     }
 }
 
-/* Medium priority task */
-void ut_task_mutex_2(void *arg)
+void mutex_high_priority_task(void *arg)
 {
-    tasks_cnt[1]++;
-    while(1)
-    {
-        os_delay(500);
-    }
-}
-
-/* Highest priority task */
-void ut_task_mutex_3(void *arg)
-{
-    /* Let the other tasks to lock the mutex */
+    /* Let the lowest priority task start first */
     os_delay(5);
 
-    tasks_cnt[2]++;
+    /* This task should be blocked by the lowest priority task due
+     * to the priority ceiling of the mutex */
+    tasks_cnt[UT_TASK_PRIORITY_HIGHEST]++;
 
     os_mutex_lock(MUTEX_ONE, 2000);
-    
-    tasks_cnt[2]++;
+    tasks_cnt[UT_TASK_PRIORITY_HIGHEST]++;
     ut_blocking_delay(10);
+
     while(1)
     {
         os_delay(500);
@@ -52,9 +57,20 @@ void TEST_mutexes(void)
 {
     PRINT_UT_BEGIN();
 
-    os_delay(300);
+    reset_counters();
+    os_task_start(OS_TASK_MUTEX_3);
+    os_task_start(OS_TASK_MUTEX_1);
 
-    TEST_ASSERT_EQUAL(1, tasks_cnt[0]);
-    TEST_ASSERT_EQUAL(1, tasks_cnt[1]);
-    TEST_ASSERT_EQUAL(2, tasks_cnt[2]);
+    os_delay(10);
+    /* Only the lowest priority task should have executed due to the mutex priority ceiling */
+    TEST_ASSERT_EQUAL(2, tasks_cnt[UT_TASK_PRIORITY_LOWEST]);
+    TEST_ASSERT_EQUAL(0, tasks_cnt[UT_TASK_PRIORITY_HIGHEST]);
+    os_delay(15);
+    /* The highest priority task should have executed after the lowest priority task unlocked the mutex */
+    TEST_ASSERT_EQUAL(2, tasks_cnt[UT_TASK_PRIORITY_LOWEST]);
+    TEST_ASSERT_EQUAL(2, tasks_cnt[UT_TASK_PRIORITY_HIGHEST]);
+    os_delay(25);
+    /* The lowest priority task should have executed again after the highest priority task unlocked the mutex */
+    TEST_ASSERT_EQUAL(3, tasks_cnt[UT_TASK_PRIORITY_LOWEST]);
+    TEST_ASSERT_EQUAL(2, tasks_cnt[UT_TASK_PRIORITY_HIGHEST]);
 }
